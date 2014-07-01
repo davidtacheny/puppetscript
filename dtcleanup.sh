6ed9e399335b7e621f88ef1f56fe39d3e3e6b98a
davidtacheny <davidtacheny@yahoo.com>
2014-07-01 04:52:11 -0700

    change
    change

--- dtcleanup.sh
+++ dtcleanup.sh
@@ -1,6 +1,7 @@
 puppet apply /etc/puppet/manifests/pmusers.pp
 puppet apply /etc/puppet/manifests/pmyumrepo.pp
 puppet apply /etc/puppet/manifests/pmpackage.pp
+puppet apply /etc/puppet/manifests/pmfile.pp
 puppet apply /etc/puppet/manifests/pmservice.pp
 puppet apply /etc/puppet/manifests/site.pp
 rm -rf /puppet/files
